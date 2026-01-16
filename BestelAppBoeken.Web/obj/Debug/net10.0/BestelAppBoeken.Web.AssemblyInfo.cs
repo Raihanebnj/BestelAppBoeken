b510672021751941f59a6e02dba3768cfc60a9a2
kiranchaudry97 <kiran.chaudry@outlook.be>
2026-01-16 10:06:23 +0100

    services toevoegen voor database backup, appsetting.json toevoegen

--- BestelAppBoeken.Web/obj/Debug/net10.0/BestelAppBoeken.Web.AssemblyInfo.cs
+++ BestelAppBoeken.Web/obj/Debug/net10.0/BestelAppBoeken.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BestelAppBoeken.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b27461b809dfc4b3f9d7290dca727969b71425d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e72eba88d881565fb55fcf3a03be63e4255a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("BestelAppBoeken.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BestelAppBoeken.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
